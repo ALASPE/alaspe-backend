9042c6417477a68cf06ab99632e8cf45d3b8404d
wifituga <manuel.nazaret.zambrano.moran@gmail.com>
2024-06-20 09:52:29 -0500

    feat(backend): add azure server connection

--- src/database/database.js
+++ src/database/database.js
@@ -1,6 +1,19 @@
-import Sequilize from 'sequelize';
+import Sequelize from 'sequelize';
+import dotenv from 'dotenv';
+dotenv.config();
 
-export const sequelize = new Sequilize('ALASPE','postgres','alaspe2024', {
-    host: 'localhost',
-    dialect: 'postgres',
-});
+export const sequelize = new Sequelize(
+    process.env.DB_DATABASE,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD, {
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        dialect: process.env.DB_DIALECT,
+        dialectOptions: {
+            ssl: {
+                require:  true,
+                rejectAuthorized: false,
+            }
+        },
+    }
+);
